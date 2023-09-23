620f1d0d0166c8638895bdc30df432bc459e9c54
Chris Mann <chriskmann32@gmail.com>
2023-09-23 16:02:22 -0500

    Removing Unused Imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import { RouterProvider, Routes, Route, HashRouter } from 'react-router-dom';
+import './index.css';
 import Homepage from './pages/Homepage';
-import Navigation from './pages/Navigation';
 import Ingredients from './pages/Ingredients';
 import Instructions from './pages/Instructions';
+import Navigation from './pages/Navigation';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
