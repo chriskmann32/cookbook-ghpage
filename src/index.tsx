9834d569feeb2a7299d07528a617590756eb723b
Chris Mann <chriskmann32@gmail.com>
2023-10-08 15:46:15 -0500

    Not In Old Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import Homepage from './pages/Homepage';
-import Ingredients from './pages/Ingredients';
-import Instructions from './pages/Instructions';
-import Navigation from './pages/Navigation';
-import {useState} from 'react'
-
-const [ids, setIds] = useState<String[]>();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
